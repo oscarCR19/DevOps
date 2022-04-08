24bd906924bc1572a6278cb670e0f4374f1ff896
Ana Solis <ananiris@hotmail.com>
2022-04-07 18:55:11 -0600

    Se escribe codigo nuevo

--- DevOps/Program.cs
+++ DevOps/Program.cs
@@ -11,7 +11,13 @@ namespace DevOps
     {
         static void Main(string[] args)
         {
-            
-        }
+            Console.Title = "Mini Calculadora";
+            string resp = "";
+            do
+            {
+                int valor1 = 0; //Almacena el primer valor ingresado.
+                int valor2 = 0;
+                int R = 0;
+            }
     }
 }
